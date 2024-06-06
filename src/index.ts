3f1c6e203ddafc2d0d0dd633db3e9a4a8d04b994
Tim Roepke <8bitluigi001@gmail.com>
2024-06-06 23:05:14 +0200

    added save-able text fields

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,81 @@ import { ISettingRegistry } from '@jupyterlab/settingregistry';
 import { Collapser } from '@jupyterlab/ui-components';
 import { ILauncher } from '@jupyterlab/launcher';
 import { PageConfig } from '@jupyterlab/coreutils';
-import { Panel } from '@lumino/widgets';
+import { Panel, Widget } from '@lumino/widgets';
 
 import { tagslist } from './tags';
 import { CheckboxButton, CheckboxButtonHolder } from './checkbox';
-import { metadataManagerWidget } from './meta_mgr';
+
+
+function hash(item: string): string {
+  let hash = 0;
+  if (item.length == 0) return hash.toString();
+  for (let i = 0; i < item.length; i++) {
+    let char = item.charCodeAt(i);
+    hash = ((hash << 5) - hash) + char;
+    hash = hash & hash; // Convert to 32bit integer
+  }
+  return hash.toString();
+}
+
+
+class metadataManagerWidget extends Widget {
+  private inputFields: HTMLInputElement[] = [];
+
+  constructor() {
+    super();
+    this.id = "metadataManagerWidget";
+    this.title.label = 'Metadata Manager';
+    this.title.closable = true;
+    this.addClass('metadataManagerWidget');
+  }
+
+  async setUp(cwd: string, settingsRegistry: ISettingRegistry, id: string) {
+    const settings = await settingsRegistry.load(id);
+
+    // These are the important and required items, make a header for them to separate from the rest.
+    const header = document.createElement('h2');
+    header.textContent = 'Required Items:';
+    this.node.appendChild(header);
+    this.node.appendChild(document.createElement('br'));
+
+    const itemsToAdd = ['Item1', 'Item2', 'Item3', 'Item4', 'Item5'];
+    for (let item of itemsToAdd) {
+      // Put the item name first
+      const itemLabel = document.createElement('label');
+      itemLabel.textContent = item + ' ';
+      this.node.appendChild(itemLabel);
+
+      // Add input field
+      const inputField = document.createElement('input');
+      inputField.type = 'text';
+      inputField.placeholder = item;
+
+      const savedState = settings.get(cwd + '-' + item).composite as string;
+      console.log('Saved state:', savedState);
+      console.log('Item:', cwd + '-' + item);
+      if (savedState !== null) {
+        inputField.value = savedState;
+      }
+
+      this.inputFields.push(inputField);
+      this.node.appendChild(inputField);
+      this.node.appendChild(document.createElement('br'));
+    }
+  }
+
+  getInputValue(index: number): string {
+    return this.inputFields[index].value;
+  }
+
+  getInput(index: number): HTMLInputElement {
+    return this.inputFields[index];
+  }
+
+  getItemsLength(): number {
+    return this.inputFields.length;
+  }
+}
 
 
 /**
@@ -25,7 +95,12 @@ const plugin: JupyterFrontEndPlugin<void> = {
   activate: async (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null, launcher: ILauncher) => {
 
     if (!settingRegistry) {
-      console.log('No setting registry available for Metadata Manager extension, exiting.');
+      console.error('No setting registry available for Metadata Manager extension, exiting.');
+      return;
+    }
+
+    if (!launcher) {
+      console.error('No launcher available for Metadata Manager extension, exiting.');
       return;
     }
 
@@ -34,6 +109,9 @@ const plugin: JupyterFrontEndPlugin<void> = {
       console.log('naavre-extension settings loaded:', settings.composite);
 
       const CWD = PageConfig.getOption('serverRoot');
+      const CWDHash = hash(CWD);    // -1873337822
+      console.log('Current working directory:', CWD);
+      console.log('hash:', CWDHash);
 
       // This is the panel in which the extension lives.
       const myPanel = new Panel();
@@ -56,14 +134,14 @@ const plugin: JupyterFrontEndPlugin<void> = {
           const newcheckbox = new CheckboxButton(tagname);
           newcheckbox.id = tagname + '-checkbox-button';
           newcheckbox.hide();
-          let savedState = settings.get(tagname).composite as boolean;
+          let savedState = settings.get(CWDHash + tagname).composite as boolean;
           if (savedState !== undefined) { // Load the saved state of the checkbox.
             newcheckbox.checkbox.checked = savedState;
           }
 
           newcheckbox.node.onclick = () => {  // Save the state of the checkbox.
             newcheckbox.checkbox.checked = !newcheckbox.checkbox.checked;
-            settings.set(tagname, newcheckbox.checkbox.checked);
+            settings.set(CWDHash + tagname, newcheckbox.checkbox.checked);
             console.log('Checkbox button clicked, checked:', tagname, newcheckbox.checkbox.checked);
           }
           myPanel.addWidget(newcheckbox);
@@ -98,7 +176,20 @@ const plugin: JupyterFrontEndPlugin<void> = {
         label: 'Open metadata manager Widget',
         execute: () => {
           const widget = new metadataManagerWidget();
+          widget.setUp(CWDHash, settingRegistry, plugin.id);
           app.shell.add(widget, 'main');
+          app.shell.activateById(widget.id);
+          app.shell.currentWidget?.close();
+
+          // Make all input fields in the widget listen for changes.
+          for (let i = 0; i < widget.getItemsLength(); i++) {
+            widget.getInput(i).addEventListener('change', () => {
+              console.log('Input field changed:', widget.getInputValue(i));
+
+              settings.set(CWDHash + '-' + widget.getInput(i).placeholder, widget.getInputValue(i));
+              console.log(CWDHash + '-' + widget.getInput(i).placeholder, widget.getInputValue(i));
+            });
+          }
         }
       });
 
